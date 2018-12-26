9cdce6cce10abeafacc9d5cce7bfc21d77dd8a0d
Chisato Kanauchi <kanauchi0510@gmail.com>
2018-12-26 19:16:24 +0900

    fix function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,9 +6,15 @@ let page = 1;
 moreButton.addEventListener('click', function() {
   axios.get('/api/gifs?page=' + page)
     .then(response => {
-      // HTMLを追加する
       /* Insert code here... */
+      const data = response.data;
+      const items = data.items;
+      const flg = data.last;
+      
+      // HTMLを追加する
+      output.appendChild(buildHTML(items));
       // 最後のページであれば「もっと見る」ボタンは隠す
+      flg ? moreButton.classList.add('hidden') : null;
       /* Insert code here... */
     });
   page++;
@@ -21,6 +27,14 @@ function buildHTML(items) {
   const row = document.createElement('div');
   row.className = 'columns';
   let html = '';
+  /* Insert code here... */
+  for (let i in items) {
+    const image = document.createElement('img');
+
+    image.className = 'column is-one-third';
+    image.src = items[i].url;
+    html += image.outerHTML;
+  }
   /* Insert code here... */
   row.innerHTML = html;
   return row;
