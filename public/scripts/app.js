d3cfb6f0b0c99eedd53fb5e80aeffbf0351e6ae1
tenma <logwhitesesame@gmail.com>
2018-12-27 01:39:09 +0900

    もっとみる機能実装

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,39 @@
-const moreButton = document.getElementById('more');
-const output = document.getElementById('output');
+const moreButton = document.getElementById("more");
+const output = document.getElementById("output");
 
 let page = 1;
 
-moreButton.addEventListener('click', function() {
-  axios.get('/api/gifs?page=' + page)
-    .then(response => {
-      // HTMLを追加する
-      /* Insert code here... */
-      // 最後のページであれば「もっと見る」ボタンは隠す
-      /* Insert code here... */
+moreButton.addEventListener("click", function() {
+    axios.get("/api/gifs?page=" + page).then(response => {
+        let html = output.innerHTML;
+        const items = response.data.items;
+        html += '<div class="columns">';
+        items.forEach(item => {
+            html +=
+                '<img class="column is-one-third" src="' +
+                item.url +
+                '" alt="" />';
+        });
+        html += "</div>";
+        output.innerHTML = html;
+        if (response.data.last) {
+            moreButton.classList.add("hidden");
+        }
+        /* Insert code here... */
+        // 最後のページであれば「もっと見る」ボタンは隠す
+        /* Insert code here... */
     });
-  page++;
+    page++;
 });
 
 /**
  * 一行分の要素を生成する
  */
 function buildHTML(items) {
-  const row = document.createElement('div');
-  row.className = 'columns';
-  let html = '';
-  /* Insert code here... */
-  row.innerHTML = html;
-  return row;
+    const row = document.createElement("div");
+    row.className = "columns";
+    let html = "";
+    /* Insert code here... */
+    row.innerHTML = html;
+    return row;
 }
