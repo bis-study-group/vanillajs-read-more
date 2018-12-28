09b2eb7487a64c7374e8d1fca381d84930a6d1db
Hiromichi Yamamoto <hiromichi.yamamoto@ari-jp.com>
2018-12-28 10:16:04 +0900

    もっと見る機能を作ろう

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,23 @@
-const moreButton = document.getElementById('more');
-const output = document.getElementById('output');
+const moreButton = document.getElementById("more");
+const output = document.getElementById("output");
 
 let page = 1;
 
-moreButton.addEventListener('click', function() {
-  axios.get('/api/gifs?page=' + page)
-    .then(response => {
-      // HTMLを追加する
-      /* Insert code here... */
-      // 最後のページであれば「もっと見る」ボタンは隠す
-      /* Insert code here... */
-    });
+moreButton.addEventListener("click", function() {
+  axios.get("/api/gifs?page=" + page).then(response => {
+    // HTMLを追加する
+    output.appendChild(buildHTML(response.data.items));
+    // 最後のページであれば「もっと見る」ボタンは隠す
+    /* Insert code here... */
+    if (response.data.last) {
+      moreButton.classList.add("hidden");
+      //5秒で初期化
+      setTimeout(function() {
+        moreButton.classList.remove("hidden");
+        output.textContent = null;
+      }, 5000);
+    }
+  });
   page++;
 });
 
@@ -18,10 +25,12 @@ moreButton.addEventListener('click', function() {
  * 一行分の要素を生成する
  */
 function buildHTML(items) {
-  const row = document.createElement('div');
-  row.className = 'columns';
-  let html = '';
-  /* Insert code here... */
-  row.innerHTML = html;
+  const row = document.createElement("div");
+  row.className = "columns";
+  for (let i = 0; i < 3; i++) {
+    let html = "<img class='column is-one-third' src='" + items[i].url + "'>";
+    row.innerHTML += html;
+  }
+
   return row;
 }
